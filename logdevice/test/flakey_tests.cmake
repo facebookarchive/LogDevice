15977c0f3f1f4286018be8f396168148589e5fa6
Caleb Marchent <cmarchent@fb.com>
2018-09-13 14:21:41 +0000

    Disable integration tests known to be flakey
    When tests can fail even without regression; confidence in the tests is
    undermined. While we work to address the intermittent issues with these
    tests remove them from the set so that developers get clear signal of
    weather or not their proposed changes cause regression.

---
+++ logdevice/test/flakey_tests.cmake
@@ -0,0 +1,21 @@
+# Copyright (c) 2018-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Integration tests disabled as faulty or flakey; removed from test
+# list to ensure result of CI run gives a clear signal
+set_tests_properties(
+  "DataSizeTest.RestartNode"
+  "IsLogEmptyTest.LogsTrimmedAway"
+  "IsLogEmptyTest.PartialResult"
+  "IsLogEmptyTest.RestartNode"
+  "NodeSetTest.DeferReleasesUntilMetaDataRead"
+  "NodeStatsControllerIntegrationTest/NodeStatsControllerIntegrationTest.RemoveWorstClients/true"
+  "Parametric/ReadPastGlobalLastReleasedTest.RecoveryStuck/(true,false,1-byteobject<01>)"
+  "ReadingIntegrationTest/ReadingIntegrationTest.StatisticsCallback/false"
+  "SequencerIntegrationTest.LogRemovalStressTest"
+  "SequencerIntegrationTest.MetaDataWritePreempted"
+  "SequencerIntegrationTest.SequencerIsolation"
+  PROPERTIES DISABLED TRUE)
