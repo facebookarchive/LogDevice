1b0598615b87b7e03fc7097b124bbac0af2bdbb8
Caleb Marchent <cmarchent@fb.com>
2018-09-20 00:56:03 -0700

    Update License headers to Standard-BSD for internal sources
    Summary:
    Bring the license headers of internal sources of LogDevice Standard-BSD
    as has already been done for open source files.
    
    Reviewed By: AhmedSoliman
    
    Differential Revision: D9919570
    
    fbshipit-source-id: c7bcc555cb3c4f5cb5e0a0ded719273ebbe1e547

--- build/fbcode_builder_config.py
+++ build/fbcode_builder_config.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import specs.zstd as zstd
