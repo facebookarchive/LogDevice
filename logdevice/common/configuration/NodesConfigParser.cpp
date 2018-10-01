2cfc8038a52aac559a806ae3ec42a0bdfbc9fc9c
David Vigier <dvigier@fb.com>
2018-10-01 15:51:47 -0700

    Set sequencer weight to 0 if sequencer=false in config regardless of the value of sequencer_weight
    Summary:
    Do not read the sequencer_weight attribute if sequencer=false in the config and set the weight to 0.
    Previously the code would consider a sequencer enabled if sequencer_weight > 0 even if sequencer was set to false in the config. This would cause newer clients and older servers to disagree on where a sequencer should run.
    
    Differential Revision: D10133633
    
    fbshipit-source-id: 618e29e2fa0bf0cab5ebe43211651a63ef5f6cd0

--- logdevice/common/configuration/NodesConfigParser.cpp
+++ logdevice/common/configuration/NodesConfigParser.cpp
@@ -491,19 +491,20 @@ static bool parseSequencer(const folly::dynamic& nodeMap,
   if (getBoolFromMap(nodeMap, "sequencer", sequencing_enabled)) {
     // Default weight is 1.0 or 0 depending on enable state.
     sequencer_weight = static_cast<double>(sequencing_enabled);
-    if (getDoubleFromMap(nodeMap, "sequencer_weight", sequencer_weight)) {
+    if (sequencing_enabled &&
+        getDoubleFromMap(nodeMap, "sequencer_weight", sequencer_weight)) {
       if (sequencer_weight < 0) {
         ld_error("Invalid value of 'sequencer_weight' attribute. Expected a "
                  "non-negative, floating point number");
         err = E::INVALID_CONFIG;
         return false;
       }
-      if (sequencing_enabled && sequencer_weight == 0) {
+      if (sequencer_weight == 0) {
         ld_warning("Enabled sequencer has a weight of 0. This sequencer will "
                    "not orchestrate the writes for any logs. Prefer setting "
                    "'sequencer' to false when disabling sequencing on a node.");
       }
-    } else if (err != E::NOTFOUND) {
+    } else if (sequencing_enabled && err != E::NOTFOUND) {
       ld_error("Invalid type for 'sequencer_weight' attribute. Expected a "
                "double");
       err = E::INVALID_CONFIG;
