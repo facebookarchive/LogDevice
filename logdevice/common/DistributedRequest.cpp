14aaf71f2b31e5e8ca018281a051381977b54d64
Lucas Prado Melo <lucasmelo@fb.com>
2019-04-23 08:10:26 -0700

    Add license header to DistributedRequest.{cpp,h}
    Reviewed By: AhmedSoliman
    
    Differential Revision: D15045312
    
    fbshipit-source-id: 3917071dbdfc02e547502ce7e74717886c155483

--- logdevice/common/DistributedRequest.cpp
+++ logdevice/common/DistributedRequest.cpp
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 #include "logdevice/common/DistributedRequest.h"
 
 namespace facebook { namespace logdevice {
--- logdevice/common/DistributedRequest.h
+++ logdevice/common/DistributedRequest.h
@@ -1,4 +1,10 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
+/**
+ * Copyright (c) 2017-present, Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #pragma once
 
