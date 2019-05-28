9c984d9090a23b156b7dfa9505af10af5c97230f
Vijaykumar Padmanaban <vijaykp@fb.com>
2019-05-28 16:41:16 -0700

    Fix lock-order-inversion in FileConfigSource
    Summary:
    This diff fixes another lock-order-inversion reported by TSAN. There are two locks that are acquired in different order depending on the method that gets called.
    1/ Synchronized TextConfigUpdaterImpl's lock
    2/ FileConfigSource's mutex
    
    getConfig is called from synchronized object (meaning, 1 is acquired) and getConfig itself acquires 2 (1 followed by 2)
    CheckForUpdates calls onAsyncGet(on a synchronized object) after locking its own mutex. (2 followed by 1)
    
    The fix is to release mutex in CheckForUpdates before calling onAyncGet since that does not have to be in the critical section. Only the read on mtime needs to be under a lock.
    
    Reviewed By: gdrane
    
    Differential Revision: D15457417
    
    fbshipit-source-id: 6f7e8740756b06746c5c6e52e73cee47ccafcb43

--- logdevice/common/FileConfigSource.cpp
+++ logdevice/common/FileConfigSource.cpp
@@ -10,6 +10,7 @@
 #include <errno.h>
 #include <memory>
 
+#include <folly/container/F14Map.h>
 #include <sys/stat.h>
 
 #include "logdevice/common/ConstructorFailed.h"
@@ -62,27 +63,40 @@ Status FileConfigSource::getConfig(const std::string& path, Output* out) {
 }
 
 void FileConfigSource::checkForUpdates() {
-  std::lock_guard<std::mutex> guard(mutex_);
-  for (auto& entry : mtimes_) {
-    const std::string& path = entry.first;
-    mtime_t mtime;
-    int rv = FileConfigSource::stat_mtime(path.c_str(), &mtime);
-    if (rv != 0 || mtime == entry.second) {
-      continue;
+  // We accumulate the output for the paths for which
+  // we detect the config change and then call the
+  // onAsyncGet callback for each of them. This is
+  // done this way because the async_cb_ could be
+  // folly::Synchronized and TSAN would complain about
+  // lock-order-inversion if we access async_cb_
+  // while holding on to `mutex_`
+  folly::F14NodeMap<std::string, Output> result;
+  {
+    std::lock_guard<std::mutex> guard(mutex_);
+    for (auto& entry : mtimes_) {
+      const std::string& path = entry.first;
+      mtime_t mtime;
+      int rv = FileConfigSource::stat_mtime(path.c_str(), &mtime);
+      if (rv != 0 || mtime == entry.second) {
+        continue;
+      }
+      entry.second = mtime;
+      ld_info("Change detected in config file %s, mtime = %ld",
+              path.c_str(),
+              mtime.count());
+      std::string contents =
+          configuration::parser::readFileIntoString(path.c_str());
+      if (!contents.empty()) {
+        Output output;
+        output.contents = std::move(contents);
+        output.mtime =
+            std::chrono::duration_cast<std::chrono::milliseconds>(mtime);
+        result[path] = std::move(output);
+      }
     }
-    entry.second = mtime;
-    ld_info("Change detected in config file %s, mtime = %ld",
-            path.c_str(),
-            mtime.count());
-    std::string contents =
-        configuration::parser::readFileIntoString(path.c_str());
-    if (!contents.empty()) {
-      Output output;
-      output.contents = std::move(contents);
-      output.mtime =
-          std::chrono::duration_cast<std::chrono::milliseconds>(mtime);
-      async_cb_->onAsyncGet(this, path, E::OK, std::move(output));
-    }
+  }
+  for (auto& kv : result) {
+    async_cb_->onAsyncGet(this, kv.first, E::OK, std::move(kv.second));
   }
 }
 
