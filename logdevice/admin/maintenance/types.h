987ebad5ef72f39e63e73203c3ff1ed1decf0498
Ahmed Soliman <asoli@fb.com>
2019-05-29 02:35:49 -0700

    Introducing MaintenanceError exception type
    Summary: This adds a wrapper error type that will be commonly used in maintenance API code. This wraps a number of maintenance related errors into one Exception type.
    
    Reviewed By: MohamedBassem
    
    Differential Revision: D15468849
    
    fbshipit-source-id: 3498809199a776348c8415d49eac1fe1d65b4149

--- logdevice/admin/maintenance/types.h
+++ logdevice/admin/maintenance/types.h
@@ -9,6 +9,7 @@
 
 #include "logdevice/admin/if/gen-cpp2/admin_types.h"
 #include "logdevice/admin/maintenance/gen-cpp2/MaintenanceDelta_types.h"
+#include "logdevice/include/Err.h"
 
 namespace facebook { namespace logdevice { namespace maintenance {
 using ShardOperationalState = thrift::ShardOperationalState;
@@ -22,5 +23,29 @@ using ClusterMaintenanceState = thrift::ClusterMaintenanceState;
 // The user string that identifies the maintenances
 // triggered by logdevice internally
 constexpr static folly::StringPiece INTERNAL_USER{"LOGDEVICE_INTERNAL_USER"};
+
+/**
+ * An exception type used to encapsulate the various thrift exceptions related
+ * to maintenance operations. This is used in cases where we want to return a
+ * folly::Expected<T, E> where E is one of the several expected error types
+ * according to the thrift spec.
+ */
+class MaintenanceError : public std::exception {
+ public:
+  MaintenanceError(Status st) noexcept : st_(st), msg_(error_description(st)) {}
+  MaintenanceError(Status st, std::string message) noexcept
+      : st_(st), msg_(message) {}
+  Status getStatus() const {
+    return st_;
+  }
+  virtual const char* what() const noexcept {
+    return msg_.c_str();
+  }
+  virtual ~MaintenanceError() {}
+
+ private:
+  Status st_;
+  std::string msg_;
+};
 
 }}} // namespace facebook::logdevice::maintenance
