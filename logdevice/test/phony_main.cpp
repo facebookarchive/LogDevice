22022de3483189902854ef6e1735f3516b90de69
Ahmed Soliman <asoli@fb.com>
2019-06-01 07:11:35 -0700

    pinning fmt/rocksdb and fix integration tests initialization (#82)
    Summary:
    - Rocksdb was correctly pinned to 5.18, we need to go up to 6.1
    - fbthrift needs older fmt, pinning that similar to how fbcode_builder does it.
    - Fixes integration tests folly::init initialisation
    Pull Request resolved: https://github.com/facebookincubator/LogDevice/pull/82
    
    Reviewed By: MohamedBassem
    
    Differential Revision: D15592574
    
    Pulled By: AhmedSoliman
    
    fbshipit-source-id: cb853480784cda9dd1763c2190ef8edee147a0a8

--- logdevice/test/phony_main.cpp
+++ logdevice/test/phony_main.cpp
@@ -5,4 +5,12 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-int main() {}
+
+#include <folly/init/Init.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  folly::init(&argc, &argv);
+  return RUN_ALL_TESTS();
+}
