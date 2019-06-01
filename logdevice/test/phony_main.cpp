bdd2bb942e3376154da6ee0f0984258179b9c85a
Ahmed Soliman <asoli@fb.com>
2019-06-01 10:55:20 +0000

    Initialize folly::init

--- logdevice/test/phony_main.cpp
+++ logdevice/test/phony_main.cpp
@@ -5,4 +5,13 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-int main() {}
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <folly/init/Init.h>
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  folly::init(&argc, &argv);
+  return RUN_ALL_TESTS();
+}
