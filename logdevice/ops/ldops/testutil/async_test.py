f3f310f8992c894d134a2ed5aee3f876530a54dc
Sergei Puzyrev <srg@fb.com>
2019-06-20 09:43:35 -0700

    Add async_test decorator to ldops testutil
    Summary: Just add async_test decorator because we can't use libfb-based one
    
    Reviewed By: ylectric
    
    Differential Revision: D15824666
    
    fbshipit-source-id: c4b2f1ffc83d10aa47847ab27fe2c227f3e41fb5

---
+++ logdevice/ops/ldops/testutil/async_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import asyncio
+
+
+def async_test(f):
+    def wrapper(*args, **kwargs):
+        coro = asyncio.coroutine(f)
+        future = coro(*args, **kwargs)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(future)
+
+    return wrapper
