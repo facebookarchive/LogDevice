4cc773f0e77bba2bfa914ee9b381d877cafcbfba
MohamedBassem <medoox240@gmail.com>
2019-07-15 06:28:26 -0700

    Print dlopen errors on failure (#95)
    Summary:
    dlopen fails without specifying the failure reason. Let's print the error to help with debugging. Example after the fix:
    ```
    ubuntu@ip-172-31-29-168 ~/LogdeviceWorkspace/Logdevice/_build [dlopen-error] $ LD_LOAD_PLUGINS=/data/LogdeviceWorkspace/logdevice-prometheus/_build/lib/libldprometheus.so ./bin/logdeviced
    terminate called after throwing an instance of 'std::runtime_error'
      what():  Failed to dlopen '/data/LogdeviceWorkspace/logdevice-prometheus/_build/lib/libldprometheus.so': /data/LogdeviceWorkspace/logdevice-prometheus/_build/lib/libldprometheus.so: undefined symbol: _ZN8facebook9logdevice3dbg11customLogFnE
    Aborted (core dumped)
    ```
    Pull Request resolved: https://github.com/facebookincubator/LogDevice/pull/95
    
    Reviewed By: tau0
    
    Differential Revision: D16259719
    
    Pulled By: MohamedBassem
    
    fbshipit-source-id: b5dbde8df1ec20b4ee884f84b237415e986fd58f

--- logdevice/common/plugin/DynamicPluginLoader.cpp
+++ logdevice/common/plugin/DynamicPluginLoader.cpp
@@ -48,8 +48,8 @@ PluginVector DynamicPluginLoader::getPlugins() {
     // available to resolve references in subsequently loaded libraries."
     void* handle = dlopen(path.c_str(), RTLD_LOCAL | RTLD_NOW);
     if (handle == nullptr) {
-      throw std::runtime_error(
-          folly::sformat("Failed to dlopen '{}'", path.string()));
+      throw std::runtime_error(folly::sformat(
+          "Failed to dlopen '{}': {}", path.string(), dlerror()));
     }
     void* ptr = dlsym(handle, kLogDevicePluginSymbolName);
     if (ptr == nullptr) {
