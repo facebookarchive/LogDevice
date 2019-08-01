8969be6724af72e63fb633969e6fc5e3ef1fed7a
Sergei Puzyrev <srg@fb.com>
2019-07-31 22:44:54 -0700

    Add MaintenanceOverallStatus
    Summary: We need synthetic MaintenanceOverallStatus for a while to define is maintenance completed or not
    
    Reviewed By: ylectric
    
    Differential Revision: D16516087
    
    fbshipit-source-id: 25543d19ccf2480a0337ffb8edc202b8842f29d9

---
+++ logdevice/ops/ldops/types/maintenance_overall_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from enum import Enum
+
+
+class MaintenanceOverallStatus(Enum):
+    INVALID = 0
+    IN_PROGRESS = 1
+    BLOCKED = 2
+    COMPLETED = 3
