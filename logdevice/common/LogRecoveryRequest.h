c183cd9edb96e58850a85eb45dbb2564c404c4de
Mohamed Bassem <mbassem@fb.com>
2019-10-08 06:29:31 -0700

    Move RetryHandler to LogRecoveryRequest
    Summary: The class `RetryHandler` was in common and had a pretty generic name although it's not really that generic. It's only used by the LogRecoveryRequest and only works on shards. I'm planning to implement a generic RetryHandler, so I'm moving this class to an anonymous namespace in LogRecoveryRequest instead.
    
    Reviewed By: mcrnic
    
    Differential Revision: D17736152
    
    fbshipit-source-id: c203d71f285598119d199ba2f3aa51dab8496c8f

--- logdevice/common/LogRecoveryRequest.h
+++ logdevice/common/LogRecoveryRequest.h
@@ -18,7 +18,6 @@
 #include "logdevice/common/MetaDataLogReader.h"
 #include "logdevice/common/Request.h"
 #include "logdevice/common/RequestType.h"
-#include "logdevice/common/RetryHandler.h"
 #include "logdevice/common/SocketCallback.h"
 #include "logdevice/common/event_log/EventLogRebuildingSet.h"
 #include "logdevice/common/protocol/SEALED_Message.h"
@@ -26,6 +25,10 @@
 #include "logdevice/common/types_internal.h"
 
 namespace facebook { namespace logdevice {
+
+namespace {
+class ShardRetryHandler;
+}
 
 /**
  * @file This request is a log recovery state machine. It's created and started
@@ -493,9 +496,9 @@ class LogRecoveryRequest : public Request,
   // to and received a SEALED(OK) to the node at this offset in the cluster.
   std::unordered_map<ShardID, NodeStatus, ShardID::Hash> node_statuses_;
 
-  // RetryHandler object responsible for resending SEAL messages when sending
-  // fails.
-  std::unique_ptr<RetryHandler> seal_retry_handler_;
+  // ShardRetryHandler object responsible for resending SEAL messages when
+  // sending fails.
+  std::unique_ptr<ShardRetryHandler> seal_retry_handler_;
 
   // timer used for periodically checking and scheduling retries on node that
   // has not yet been sealed
