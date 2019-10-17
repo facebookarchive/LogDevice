4a1d9a1df14af3f1439472414501f49a688a402a
Mateusz Puczel <mpuczel@fb.com>
2019-10-17 03:42:43 -0700

    Add integration test for checkpointing
    Summary:
    Implemented integration test for checkpointing that creates a checkpointed reader that reads some logs, saves checkpoints, then restarts and continues reading from checkpoint.
    
    There is one test for each sync and async checkpointed reader.
    
    Reviewed By: MohamedBassem
    
    Differential Revision: D17929283
    
    fbshipit-source-id: db173c9a6b5e734db0ec3cede83001a246fa203c

---
+++ logdevice/lib/checkpointing/test/CheckpointingIntegrationTest.cpp
@@ -0,0 +1,184 @@
+/**
+ * Copyright (c) 2019-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include "logdevice/common/test/TestUtil.h"
+#include "logdevice/include/CheckpointStoreFactory.h"
+#include "logdevice/include/CheckpointedReaderFactory.h"
+#include "logdevice/include/Client.h"
+#include "logdevice/include/SyncCheckpointedReader.h"
+#include "logdevice/test/utils/IntegrationTestUtils.h"
+
+namespace facebook { namespace logdevice {
+
+class CheckpointingIntegrationTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    cluster_ = IntegrationTestUtils::ClusterFactory().setNumLogs(2).create(4);
+    client_ = cluster_->createClient();
+    temp_dir_ = createTemporaryDir("checkpointTestForStore");
+    store_path_ = temp_dir_->path().generic_string();
+  }
+  void TearDown() override {}
+
+  void appendSampleRecords(logid_t log_id, uint32_t records_total) {
+    for (int i = 0; i < records_total; ++i) {
+      ASSERT_NE(LSN_INVALID, client_->appendSync(log_id, "payload"));
+    }
+  }
+
+  std::unique_ptr<IntegrationTestUtils::Cluster> cluster_;
+  std::shared_ptr<Client> client_;
+  std::unique_ptr<SyncCheckpointedReader> reader_;
+  std::unique_ptr<AsyncCheckpointedReader> async_reader_;
+  std::unique_ptr<folly::test::TemporaryDirectory> temp_dir_;
+  std::string store_path_;
+};
+
+TEST_F(CheckpointingIntegrationTest, SyncReaderTest) {
+  cluster_->waitForRecovery();
+  auto reader = client_->createReader(2);
+  auto store =
+      CheckpointStoreFactory().createFileBasedCheckpointStore(store_path_);
+  reader_ = CheckpointedReaderFactory().createSyncCheckpointedReader(
+      "customer", std::move(reader), std::move(store), {});
+
+  uint32_t records_total = 100;
+  uint32_t records_break = 40;
+  appendSampleRecords(logid_t(1), records_break);
+  appendSampleRecords(logid_t(2), records_break);
+
+  ASSERT_EQ(0,
+            reader_->startReadingFromCheckpoint(
+                logid_t(1), client_->getTailLSNSync(logid_t(1))));
+  ASSERT_EQ(0,
+            reader_->startReadingFromCheckpoint(
+                logid_t(2), client_->getTailLSNSync(logid_t(2))));
+
+  ssize_t total_read = 0;
+  auto read_until_no_data = [this, &total_read, records_total]() {
+    std::vector<std::unique_ptr<DataRecord>> data_out;
+    GapRecord gap_out;
+    while (true) {
+      data_out.clear();
+      auto nread = reader_->read(records_total, &data_out, &gap_out);
+      if (nread == -1 && err == E::GAP) {
+        continue;
+      }
+      ASSERT_NE(-1, nread);
+      if (nread == 0) {
+        break;
+      }
+      total_read += nread;
+    }
+  };
+
+  read_until_no_data();
+
+  // We multiply by 2 because while read from two logs.
+  EXPECT_EQ(2 * records_break, total_read);
+
+  EXPECT_EQ(Status::OK, reader_->syncWriteCheckpoints());
+
+  reader = client_->createReader(2);
+  store = CheckpointStoreFactory().createFileBasedCheckpointStore(store_path_);
+  reader_ = CheckpointedReaderFactory().createSyncCheckpointedReader(
+      "customer", std::move(reader), std::move(store), {});
+
+  appendSampleRecords(logid_t(1), records_total - records_break);
+  appendSampleRecords(logid_t(2), records_total - records_break);
+
+  ASSERT_EQ(0,
+            reader_->startReadingFromCheckpoint(
+                logid_t(1), client_->getTailLSNSync(logid_t(1))));
+  ASSERT_EQ(0,
+            reader_->startReadingFromCheckpoint(
+                logid_t(2), client_->getTailLSNSync(logid_t(2))));
+
+  read_until_no_data();
+
+  EXPECT_EQ(2 * records_total, total_read);
+}
+
+TEST_F(CheckpointingIntegrationTest, AsyncReaderTest) {
+  cluster_->waitForRecovery();
+  auto reader = client_->createAsyncReader(2);
+  auto store =
+      CheckpointStoreFactory().createFileBasedCheckpointStore(store_path_);
+  async_reader_ = CheckpointedReaderFactory().createAsyncCheckpointedReader(
+      "customer", std::move(reader), std::move(store), {});
+  uint32_t records_total = 100;
+  uint32_t records_break = 40;
+
+  appendSampleRecords(logid_t(1), records_break);
+  appendSampleRecords(logid_t(2), records_break);
+
+  std::atomic_int32_t total_read = 0;
+  folly::Baton<> done_1, done_2;
+
+  auto record_cb = [&total_read](std::unique_ptr<DataRecord>&) {
+    total_read++;
+    return true;
+  };
+
+  auto done_cb = [&done_1, &done_2](logid_t log_id) {
+    if (log_id == logid_t(1)) {
+      done_1.post();
+    } else {
+      done_2.post();
+    }
+  };
+
+  async_reader_->setRecordCallback(record_cb);
+  async_reader_->setDoneCallback(done_cb);
+
+  async_reader_->startReadingFromCheckpoint(
+      logid_t(1),
+      [](Status status) { ASSERT_EQ(Status::OK, status); },
+      client_->getTailLSNSync(logid_t(1)));
+
+  async_reader_->startReadingFromCheckpoint(
+      logid_t(2),
+      [](Status status) { ASSERT_EQ(Status::OK, status); },
+      client_->getTailLSNSync(logid_t(2)));
+
+  done_1.wait();
+  done_1.reset();
+  done_2.wait();
+  done_2.reset();
+
+  // We multiply by 2 because while read from two logs.
+  EXPECT_EQ(2 * records_break, total_read);
+  EXPECT_EQ(Status::OK, async_reader_->syncWriteCheckpoints());
+
+  reader = client_->createAsyncReader(2);
+  store = CheckpointStoreFactory().createFileBasedCheckpointStore(store_path_);
+  async_reader_ = CheckpointedReaderFactory().createAsyncCheckpointedReader(
+      "customer", std::move(reader), std::move(store), {});
+
+  appendSampleRecords(logid_t(1), records_total - records_break);
+  appendSampleRecords(logid_t(2), records_total - records_break);
+
+  async_reader_->setRecordCallback(record_cb);
+  async_reader_->setDoneCallback(done_cb);
+
+  async_reader_->startReadingFromCheckpoint(
+      logid_t(1),
+      [](Status status) { ASSERT_EQ(Status::OK, status); },
+      client_->getTailLSNSync(logid_t(1)));
+  async_reader_->startReadingFromCheckpoint(
+      logid_t(2),
+      [](Status status) { ASSERT_EQ(Status::OK, status); },
+      client_->getTailLSNSync(logid_t(2)));
+
+  done_1.wait();
+  done_2.wait();
+  EXPECT_EQ(2 * records_total, total_read);
+}
+}} // namespace facebook::logdevice
