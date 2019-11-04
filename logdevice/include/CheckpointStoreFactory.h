bcfce7b9700a70763f461dc6534174b73b9286ab
Mateusz Puczel <mpuczel@fb.com>
2019-11-04 08:32:43 -0800

    Add RSM based checkpointstore and zookeeper based checkpointstore to CheckpointStore factory.
    Summary: Implemented factory functions that create the checkpoint store based on Zookeeper and RSM.
    
    Reviewed By: MohamedBassem
    
    Differential Revision: D18205180
    
    fbshipit-source-id: a3490a8056bb5e7908bdccf52bb9e9600cefe941

--- logdevice/include/CheckpointStoreFactory.h
+++ logdevice/include/CheckpointStoreFactory.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "logdevice/include/CheckpointStore.h"
+#include "logdevice/include/Client.h"
 
 namespace facebook { namespace logdevice {
 /*
@@ -19,10 +20,35 @@ class CheckpointStoreFactory {
    * Creates a file based CheckpointStore.
    *
    * @param root_path: a path to a root file, where the checkpoints are
-   * stored.
+   *   stored.
    */
   std::unique_ptr<CheckpointStore>
   createFileBasedCheckpointStore(std::string root_path);
+
+  /**
+   * Creates a zookeeper based CheckpointStore.
+   *
+   * @param client: the instance of the client which contains the zookeeper
+   *   config.
+   */
+  std::unique_ptr<CheckpointStore>
+  createZookeeperBasedCheckpointStore(std::shared_ptr<Client>& client);
+
+  /**
+   * Creates a Replicated State Machine based CheckpointStore. The checkpoints
+   * are stored in a given log as records. One record represents a single
+   * update of the checkpoint for a given customer id. The entire log is
+   * a sequence of updates which forms the current state of the store.
+   *
+   * @param client: the instance of the client which will read and write the
+   *   checkpoints.
+   * @param log_id: the id of the log, where the checkpoints will be stored.
+   * @param stop_timeout: timeout for the RSM to stop after calling shutdown.
+   */
+  std::unique_ptr<CheckpointStore>
+  createRSMBasedCheckpointStore(std::shared_ptr<Client>& client,
+                                logid_t log_id,
+                                std::chrono::milliseconds stop_timeout);
 };
 
 }} // namespace facebook::logdevice
--- logdevice/lib/checkpointing/CheckpointStoreFactory.cpp
+++ logdevice/lib/checkpointing/CheckpointStoreFactory.cpp
@@ -8,7 +8,12 @@
 #include "logdevice/include/CheckpointStoreFactory.h"
 
 #include "logdevice/common/FileBasedVersionedConfigStore.h"
+#include "logdevice/common/RSMBasedVersionedConfigStore.h"
 #include "logdevice/common/VersionedConfigStore.h"
+#include "logdevice/common/ZookeeperVersionedConfigStore.h"
+#include "logdevice/common/plugin/PluginRegistry.h"
+#include "logdevice/common/plugin/ZookeeperClientFactory.h"
+#include "logdevice/lib/ClientImpl.h"
 #include "logdevice/lib/checkpointing/CheckpointStoreImpl.h"
 
 namespace facebook { namespace logdevice {
@@ -17,6 +22,46 @@ std::unique_ptr<CheckpointStore>
 CheckpointStoreFactory::createFileBasedCheckpointStore(std::string root_path) {
   auto versioned_config_store = std::make_unique<FileBasedVersionedConfigStore>(
       root_path, CheckpointStoreImpl::extractVersion);
+  return std::make_unique<CheckpointStoreImpl>(
+      std::move(versioned_config_store));
+}
+
+std::unique_ptr<CheckpointStore>
+CheckpointStoreFactory::createZookeeperBasedCheckpointStore(
+    std::shared_ptr<Client>& client) {
+  ClientImpl* client_impl = dynamic_cast<ClientImpl*>(client.get());
+  ld_check(client_impl);
+
+  std::shared_ptr<ZookeeperClientFactory> zookeeper_client_factory =
+      client_impl->getProcessor()
+          .getPluginRegistry()
+          ->getSinglePlugin<ZookeeperClientFactory>(
+              PluginType::ZOOKEEPER_CLIENT_FACTORY);
+  auto zookeeper_config = client_impl->getConfig()->getZookeeperConfig();
+  auto zookeeper_client =
+      zookeeper_client_factory->getClient(*zookeeper_config);
+  if (!zookeeper_client) {
+    ld_error("Failed to create a zookeeper client in CheckpointStoreFactory");
+    return nullptr;
+  }
+  auto versioned_config_store = std::make_unique<ZookeeperVersionedConfigStore>(
+      CheckpointStoreImpl::extractVersion, std::move(zookeeper_client));
+  return std::make_unique<CheckpointStoreImpl>(
+      std::move(versioned_config_store));
+}
+
+std::unique_ptr<CheckpointStore>
+CheckpointStoreFactory::createRSMBasedCheckpointStore(
+    std::shared_ptr<Client>& client,
+    logid_t log_id,
+    std::chrono::milliseconds stop_timeout) {
+  ClientImpl* client_impl = dynamic_cast<ClientImpl*>(client.get());
+  ld_check(client_impl);
+  auto versioned_config_store = std::make_unique<RSMBasedVersionedConfigStore>(
+      log_id,
+      CheckpointStoreImpl::extractVersion,
+      &client_impl->getProcessor(),
+      stop_timeout);
   return std::make_unique<CheckpointStoreImpl>(
       std::move(versioned_config_store));
 }
