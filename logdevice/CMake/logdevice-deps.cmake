7faba4e0f4ba79ad1c616d6c8efd6405712714f8
Ahmed Soliman <asoli@fb.com>
2019-11-29 04:31:06 -0800

    Enabling shared libs of folly and thrift in OSS
    Summary:
    - Allowing Cython 0.28.X to work
    - Switching to folly's shared library setup instead of static linking
    
    Reviewed By: MohamedBassem
    
    Differential Revision: D18685008
    
    fbshipit-source-id: 9055ccdfb6d5fd54882095f4b8c2b32d07fce04c

--- logdevice/CMake/logdevice-deps.cmake
+++ logdevice/CMake/logdevice-deps.cmake
@@ -9,7 +9,7 @@ set(LD_PYTHON_VERSION 3.5 CACHE STRING "Python version")
 find_package(PythonInterp ${LD_PYTHON_VERSION} REQUIRED)
 find_package(PythonLibs ${LD_PYTHON_VERSION} REQUIRED)
 if(thriftpy3)
-  find_package(Cython 0.29 REQUIRED)
+  find_package(Cython 0.28 REQUIRED)
 endif()
 set(_boost_py_component1
 	    python${PYTHON_VERSION_MAJOR})
