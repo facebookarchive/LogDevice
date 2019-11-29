6dada7335bff8799b497061c0c68a3bd3b3860b7
Ahmed Soliman <asoli@fb.com>
2019-11-29 04:31:06 -0800

    Fixing fbthrift and folly cython bindings build
    Summary:
    This diff addresses a collection of issues with our thrift-py3 build story in open source.
    - Cython 0.29 bug https://github.com/cython/cython/issues/2985 prevents us from loading the generated binary python extensions. I've tested 0.28.6 and it works perfectly.
    - Adds ssl to the extensions (since we need SSLContext in get_client, we must have access to the module)
    - No need to special case folly_pic. There is really no easy way to get the python bindings to work without using shared library libfolly.so. So, In our build (logdevice) we will build folly as a shared library, this also reduces the complexity everywhere.
    - We also need iobuf as extension for thrift to work, added that to setup.py
    - Refactored some of the CMake code that had hardcoded file names to use globbing to future-proof this as much as possible.
    - Moved `python/lang/cast.pxd` to match the python module name `folly.cast`, so now it lives in `python/cast.pxd`. This simplifies the globbing as well.
    - Moved `setup.py` to live in the `/python` directory.
    - Added `*.py` in setup.py to pickup any raw python code (including `__init__.py`)
    
    Reviewed By: yfeldblum
    
    Differential Revision: D18685009
    
    fbshipit-source-id: 749b30942a3e5e9e314b5248cc0aa90c6ac1581f

--- logdevice/CMake/logdevice-functions.cmake
+++ logdevice/CMake/logdevice-functions.cmake
@@ -229,14 +229,12 @@ macro(ld_thrift_py3_library file_name services options file_path output_path inc
           PRIVATE
           "${file_name}-cpp2"
           thriftcpp2_shared
-          folly_pic
         )
       else()
         target_link_libraries(${_module_name}
           PRIVATE
           "${file_name}-cpp2"
           FBThrift::thriftcpp2_shared
-          Folly::folly_pic
         )
       endif()
       target_include_directories(${_module_name} PUBLIC ${PYTHON_INCLUDE_DIRS})
