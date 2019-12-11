c186909b04c7ce8cbde75fd07725bd8cfc2f0f71
Mohamed Bassem <mbassem@fb.com>
2019-12-11 10:51:31 -0800

    ClusterMembership: Implement a bootstrap ldshell command
    Reviewed By: AhmedSoliman
    
    Differential Revision: D18749860
    
    fbshipit-source-id: ea68210b1a65d9dcd476cff57c8c85b4d91acfe0

--- logdevice/ops/ldshell/autoload/commands/nodes_config.py
+++ logdevice/ops/ldshell/autoload/commands/nodes_config.py
@@ -13,14 +13,19 @@ import os
 import subprocess
 import tempfile
 import time
+import typing
+from textwrap import dedent
 
 import nubia
 import pygments
 import termcolor
 from ldshell.helpers import ask_prompt, confirm_prompt
+from logdevice.admin.cluster_membership.types import BootstrapClusterRequest
+from logdevice.admin.common.types import LocationScope, ReplicationProperty
 from logdevice.admin.settings.types import SettingsRequest
 from logdevice.ops import nodes_configuration_manager as ncm
 from pygments import formatters, lexers
+from thrift.py3 import RpcOptions
 
 
 class EditorError(Exception):
@@ -201,6 +206,65 @@ class NodesConfig:
                 continue
 
         return 0
+
+    @nubia.command
+    @nubia.argument(
+        "metadata_replicate_across",
+        type=typing.Mapping[str, int],
+        description=dedent(
+            """
+            Defines cross-domain replication for metadata logs. A vector of replication factors
+            at various scopes. When this option is given, replicationFactor_ is
+            optional. This option is best explained by examples:
+                - "node: 3, rack: 2" means "replicate each record to at least 3 nodes
+                in at least 2 different racks".
+                - "rack: 2" with replicationFactor_ = 3 mean the same thing.
+                - "rack: 3, region: 2" with replicationFactor_ = 4 mean "replicate
+                each record to at least 4 nodes in at least 3 different racks in at
+                least 2 different regions"
+                - "rack: 3" means "replicate each record to at least 3 nodes in
+                at least 3 different racks".
+                - "rack: 3" with replicationFactor_ = 3 means the same thing.
+             """
+        ),
+    )
+    async def bootstrap(self, metadata_replicate_across: typing.Mapping[str, int]):
+        """
+        In a newly bootstrapped cluster, this call is needed to finalize the
+        bootstrapping and allow the cluster to be used. This call will:
+        1- Enable sequencing on all the nodes that have sequencer role.
+        2- Transition storage nodes that are done provisioning into ReadWrite.
+        3- Create the initial metadata nodeset according to the passed
+            replication property.
+        4- Mark the NodesConfiguration as bootstrapped.
+
+        Calls to this function will fail if the cluster is already bootstrapped
+        or if there are not enough provisioned storage nodes to satisfy the
+        requested replication property.
+        """
+
+        replication_property: ReplicationProperty = {}
+        for scope_str, factor in metadata_replicate_across.items():
+            try:
+                scope = LocationScope[scope_str.upper()]
+                replication_property[scope] = factor
+            except KeyError:
+                termcolor.cprint(f"{scope_str} is not a valid scope", "red")
+                return 1
+
+        ctx = nubia.context.get_context()
+        async with ctx.get_cluster_admin_client() as client:
+            try:
+                await client.bootstrapCluster(
+                    request=BootstrapClusterRequest(
+                        metadata_replication_property=replication_property
+                    ),
+                    rpc_options=RpcOptions(timeout=60),
+                )
+                termcolor.cprint("Successfully bootstrapped the cluster", "green")
+            except Exception as e:
+                termcolor.cprint(str(e), "red")
+                return 1
 
     @nubia.command
     async def provision(self):
