3cf03b95ab78132b6279ff0ab9003dcbeac223f4
Mark Marchukov <bzmbx-github@yahoo.com>
2020-01-23 12:10:10 -0700

    make prometheus-cpp a submodule of LogDevice
    If ldprometheus plugin is in the main build, its external dependencies
    should be handled the same way all other LogDevice external dependencies are.
    
    Test plan:
    
    Did a clean pull of LogDevice with this change into a new directory.
    Followed the steps to rebuild LogDevice normally.
    Verified that build completes and the Prometheus plugin loads fine.

--- logdevice/plugins/prometheus/cmake/build-prometheus-cpp.cmake
+++ logdevice/plugins/prometheus/cmake/build-prometheus-cpp.cmake
@@ -4,17 +4,20 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+set(PROMETHEUS_ROOT_DIR ${LOGDEVICE_DIR}/external/prometheus-cpp)
+
 include(ExternalProject)
 
 ExternalProject_Add(prometheus
-    GIT_REPOSITORY https://github.com/jupp0r/prometheus-cpp
-    GIT_TAG master
-    PREFIX "${CMAKE_CURRENT_BINARY_DIR}"
-    SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/external/prometheus-cpp"
-    CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF
-               -DENABLE_TESTING=OFF
-               -DENABLE_PUSH=OFF
-               -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+    SOURCE_DIR "${PROMETHEUS_ROOT_DIR}"
+    DOWNLOAD_COMMAND ""
+    CMAKE_ARGS
+        -DBUILD_SHARED_LIBS=OFF
+        -DENABLE_TESTING=OFF
+        -DENABLE_PUSH=OFF
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+        -DCMAKE_PREFIX_PATH=${LOGDEVICE_STAGING_DIR}/usr/local	
     INSTALL_COMMAND make install DESTDIR=${LOGDEVICE_STAGING_DIR}
     )
 
@@ -31,6 +34,7 @@ message(STATUS "Prometheus Library: ${PROMETHEUS_LIBRARIES}")
 message(STATUS "Prometheus Includes: ${PROMETHEUS_INCLUDE_DIR}")
 
 mark_as_advanced(
+  PROMETHEUS_ROOT_DIR
   PROMETHEUS_LIBRARIES
   PROMETHEUS_INCLUDE_DIR
 )
