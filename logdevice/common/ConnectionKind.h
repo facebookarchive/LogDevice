dc19d5c7d187335b3d0cfb706e4929174092138b
Mahdy Nasr <mahdynasr@fb.com>
2020-06-04 21:18:42 -0700

    Create ConnectionKind enum to be included across files
    Summary: Add ConnectionKind Enum to be used withen client and server.
    
    Reviewed By: 747mmHg
    
    Differential Revision: D21815386
    
    fbshipit-source-id: affa332284bb7a9aa099d20914afe33c9cdacfc2

---
+++ logdevice/common/ConnectionKind.h
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#pragma once
+
+namespace facebook { namespace logdevice {
+/**
+ * @file ConnectionKind enum represents the kind of connection is
+ *       defined by the server-side port, so all connection coming to
+ *       the same port are of the same kind and it is different from kinds
+ *       of other ports.
+ */
+
+enum class ConnectionKind : uint8_t {
+  DATA,
+  DATA_SSL,
+  GOSSIP,
+  SERVER_TO_SERVER,
+  MAX,
+};
+
+}} // namespace facebook::logdevice
